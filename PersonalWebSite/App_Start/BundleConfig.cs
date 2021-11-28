9df2bddc4ee3e55baf57dd0e6ff777991436db1d
FatPigLiu <13534592045@163.com>
2021-11-28 18:52:16 +0800

    模板页框架实现

--- PersonalWebSite/App_Start/BundleConfig.cs
+++ PersonalWebSite/App_Start/BundleConfig.cs
@@ -23,8 +23,7 @@ namespace PersonalWebSite
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/bootstrap.css", "~/Content/Navigation-with-Button.css"));
         }
     }
 }
